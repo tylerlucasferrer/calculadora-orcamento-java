0d9316bd1f5ee992417abb9460a3fcd1a86d7145
Tyler Lucas Ferreira <tylerlucasferrer@outlook.com>
2022-12-16 08:13:44 -0300

    feat: classe ItemDeSistema

---
+++ src/main/java/com/orcamento/calculadora/ItemDeSistema.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.orcamento.calculadora;
+
+/**
+ *
+ * @author r.prado.dos.santos
+ */
+public class ItemDeSistema {
+    private String tipo;
+    private int dificuldade;
+    private int tempoEmpregado;
+
+    /**
+     * @return the tipo
+     */
+    public String getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    /**
+     * @return the dificuldade
+     */
+    public int getDificuldade() {
+        return dificuldade;
+    }
+
+    /**
+     * @param dificuldade the dificuldade to set
+     */
+    public void setDificuldade(int dificuldade) {
+        this.dificuldade = dificuldade;
+    }
+
+    /**
+     * @return the tempoEmpregado
+     */
+    public int getTempoEmpregado() {
+        return tempoEmpregado;
+    }
+
+    /**
+     * @param tempoEmpregado the tempoEmpregado to set
+     */
+    public void setTempoEmpregado(int tempoEmpregado) {
+        this.tempoEmpregado = tempoEmpregado;
+    }
+    
+    
+}
